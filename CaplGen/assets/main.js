bc3c5c3ea5c906e2237f0838c5b726ed35001d40
nihalmishra <nihal.mishra95@gmail.com>
2018-07-12 16:45:22 +0530

    added django-widget-tweaks and req.txt

--- CaplGen/assets/main.js
+++ CaplGen/assets/main.js
@@ -1,7 +1,8 @@
 // $(document).ready(function() {
 //     // do stuff when DOM is ready
-//     jQuery("a.list-group-item").click(function (e) {
-//         jQuery(this).addClass('active')
-//         // .siblings().removeClass('active');
-//     });
+//     // jQuery("a.list-group-item").click(function (e) {
+//     //     jQuery(this).addClass('active')
+//     //     // .siblings().removeClass('active');
+//     // });
+//     $("#id_xlfile").attr('style="border: 1px solid black; padding: 5px;"')
 //  });
--- CaplGen/dzip/views.py
+++ CaplGen/dzip/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from dzip.models import UploadForm, Upload
 from django.urls import reverse
 
 def index(request):
     excel_file = UploadForm()
-    return render(request,'dzip/dzip.html',{'form': excel_file})
+    return render(request,'dzip/dzip.html',
+    {'form': excel_file}
+    )
 
 def generate(request):
     if request.method=="POST":
         excel_file = UploadForm(request.POST, request.FILES)
         if excel_file.is_valid():
             excel_file.save()
-            return HttpResponseRedirect(reverse('dzip:index'))
+            return redirect(reverse('dzip:index'))
     else:
         excel_file = UploadForm()
     return render(request,'dzip/dzip.html', {'form': excel_file})
-# def upload(request):
+# def generate(request):
 #     if "GET" == request.method:
 #         return render(request,'dzip.html',{})
 #     else:
 #         excel_file = request.FILES["excel_file"]
-#         wb = openpyxl.load_workbook(excel_file)
-#         wb.save("media/userdata.xlsx")
+#         with open('some/file/name.txt', 'wb+') as destination:
+#         for chunk in excel_file.chunks():
+#             destination.write(chunk)
+#         # wb = openpyxl.load_workbook(excel_file)
+#         # wb.save("media/userdata.xlsx")
 #     return redirect('dzip:index')
