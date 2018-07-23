5dd0ebaf0a919fcaa9002ce3a09bde02c7f6f053
shantanu1395 <shantanu1395@gmail.com>
2018-07-23 15:26:38 +0530

    Form changes, admin setup

--- CaplGen/dzip/models.py
+++ CaplGen/dzip/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.forms import ModelForm
+from django.utils import timezone
+import pytz
+
+CHOICES = (
+    ('capl','CAPL'),
+    ('capl-xml', 'CAPL-XML'),
+)
 
 class Upload(models.Model):
 	xlfile = models.FileField(upload_to="files/")    
 	upload_date = models.DateTimeField(auto_now_add=True)
+	sheet_name = models.CharField(max_length=40,default='')
+	option = models.CharField(max_length=8, choices=CHOICES, default='capl-xml')
+
+	def __str__(self):
+		return (str(self.xlfile) + " " + str(timezone.now()))
 
 # FileUpload form class.
 class UploadForm(ModelForm):
 	class Meta:
 		model = Upload
-		fields = ('xlfile',)
+		fields = ('xlfile', 'sheet_name', 'option')
--- CaplGen/dzip/views.py
+++ CaplGen/dzip/views.py
@@ -12,11 +12,12 @@ def generate(request):
     if request.method=="POST":
         excel_file = UploadForm(request.POST, request.FILES)
         if excel_file.is_valid():
-            excel_file.save()
+            excel_file.save()       
             return redirect(reverse('dzip:index'))
     else:
         excel_file = UploadForm()
     return render(request,'dzip/dzip.html', {'form': excel_file})
+
 # def generate(request):
 #     if "GET" == request.method:
 #         return render(request,'dzip.html',{})
